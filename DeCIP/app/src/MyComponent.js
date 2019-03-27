a479a35530253dd1cb3a46a21cf127be4c01bef9
Arun V Kumar <arunvkumar7@live.com>
2019-03-27 13:19:47 +0530

    Again working

--- DeCIP/app/src/MyComponent.js
+++ DeCIP/app/src/MyComponent.js
@@ -1,45 +1,95 @@
-import React from "react";
+import React, {Component} from "react";
+// import PropTypes from 'prop-types'
 import {
   AccountData,
   ContractData,
   ContractForm,
 } from "drizzle-react-components";
 
-export default ({ accounts }) => (
-  <div className="App">
-    <div className="section">
-      <h2>Active Account</h2>
-      <AccountData accountIndex={Math.floor(Math.random() * 10)} units="ether" precision="3" />
-    </div>
+class MyComponent extends Component
+{
 
-    <div className="section">
+  
 
-      <h2>Ideation Contest- Summary</h2>
-      <p>
-        All Submitted Ideas.
-      </p>
-      <p>
-        <strong>Total Ideas : </strong>
-        <ContractData contract="IdeationContest" method="TotalIdeas" />
-      </p>
-      <div>
-        <ContractData contract="IdeationContest" method="getIdeas" />
+  render() {
+    return (
+      <div className="App">
+        <div className="section">
+          <h2>Active Account</h2>
+          <AccountData accountIndex={Math.floor(Math.random() * 10)} units="ether" precision="3" />
+        </div>
+    
+        <div className="section">
+    
+          <h2>Ideation Contest- Summary</h2>
+          <p>
+            All Submitted Ideas.
+          </p>
+          <p>
+            <strong>Total Ideas : </strong>
+            <ContractData contract="IdeationContest" method="TotalIdeas" />
+          </p>
+          <div>
+            <ContractData contract="IdeationContest" method="getIdeas" />
+          </div>
+          <ContractForm contract="IdeationContest" method="SubmitIdea" sendArgs={{gas: 1200000, gasPrice: 40000000000}}/>
+    
+          <h2>Idea details</h2>
+          <ContractForm contract="IdeationContest" method="getIdeaDetails" />
+    
+          <h2>SimpleStorage</h2>
+          <p>
+            This shows a simple ContractData component with no arguments, along with
+            a form to set its value.
+          </p>
+          <p>
+            <strong>Stored Value: </strong>
+            <ContractData contract="SimpleStorage" method="storedData" />
+          </p>
+          <ContractForm contract="SimpleStorage" method="set" />
+        </div>
       </div>
-      <ContractForm contract="IdeationContest" method="SubmitIdea" sendArgs={{gas: 1200000, gasPrice: 40000000000}}/>
+    );
+  }
+}
 
-      <h2>Idea details</h2>
-      <ContractForm contract="IdeationContest" method="getIdeaDetails" />
+export default MyComponent;
 
-      <h2>SimpleStorage</h2>
-      <p>
-        This shows a simple ContractData component with no arguments, along with
-        a form to set its value.
-      </p>
-      <p>
-        <strong>Stored Value: </strong>
-        <ContractData contract="SimpleStorage" method="storedData" />
-      </p>
-      <ContractForm contract="SimpleStorage" method="set" />
-    </div>
-  </div>
-);
+// export default ({ accounts }) => (
+//   <div className="App">
+//     <div className="section">
+//       <h2>Active Account</h2>
+//       <AccountData accountIndex={Math.floor(Math.random() * 10)} units="ether" precision="3" />
+//     </div>
+
+//     <div className="section">
+
+//       <h2>Ideation Contest- Summary</h2>
+//       <p>
+//         All Submitted Ideas.
+//       </p>
+//       <p>
+//         <strong>Total Ideas : </strong>
+//         <ContractData contract="IdeationContest" method="TotalIdeas" />
+//       </p>
+//       <div>
+//         <ContractData contract="IdeationContest" method="getIdeas" />
+//       </div>
+//       <ContractForm contract="IdeationContest" method="SubmitIdea" sendArgs={{gas: 1200000, gasPrice: 40000000000}}/>
+
+//       <h2>Idea details</h2>
+//       <ContractForm contract="IdeationContest" method="getIdeaDetails" />
+
+//       <h2>SimpleStorage</h2>
+//       <p>
+//         This shows a simple ContractData component with no arguments, along with
+//         a form to set its value.
+//       </p>
+//       <p>
+//         <strong>Stored Value: </strong>
+//         <ContractData contract="SimpleStorage" method="storedData" />
+//       </p>
+//       <ContractForm contract="SimpleStorage" method="set" />
+//     </div>
+//   </div>
+// );
